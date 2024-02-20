5162b4fc17ca692fa4fc34a561df7c05f6dc80a3
卢照天 <2430485982@qq.com>
2024-02-20 17:43:17 +0800

    增加 Copy 全部功能

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CookiesCopy
 谷歌浏览器插件支持复制 Cookies 信息
+
+# 安装教程
+**点击扩展程序**
+![alt text](./image/2.png)
+**打开开发者模式**
+![alt text](./image/3.png)
+**然后项目直接拖进来就可**
+
+**最终效果**
+![alt text](./image/4.png)
--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,22 @@
-let time = null
+let time = null,
+  allTime = null,
+  allKeyTime = null,
+  allValueTime = null,
+  myUrlList;
+
+let successObject = {
+  text: "复制成功",
+  color: "var(--btn-text-success-color)",
+};
 
 const init = () => {
   getHtml();
-  setCopy()
+  setCopy();
 };
-
-
 
 const copy = (text) => {
   navigator.clipboard?.writeText && navigator.clipboard.writeText(text);
-}
-
-
+};
 
 const getHtml = () => {
   const cookiesList = document.getElementById("cookiesList");
@@ -25,7 +30,7 @@ const getHtml = () => {
         const { origin, hostname, url, domain } = new URL(tabs[0].url);
         title.innerText = `当前页面：${hostname}`;
         chrome.cookies.getAll({ url }, (cookies) => {
-          const myUrlList = cookies.filter((item) => item.domain === hostname);
+          myUrlList = cookies.filter((item) => item.domain === hostname);
           console.log(myUrlList);
 
           if (myUrlList.length > 0) {
@@ -43,25 +48,120 @@ const getHtml = () => {
   });
 };
 
-
 const setCopy = () => {
-  const cookiesListClickAddEventListener = document.getElementById("cookiesList");
+  const cookiesListClickAddEventListener =
+    document.getElementById("cookiesList");
+  const copyAll = document.getElementById("copyAll");
+  const copyAllKey = document.getElementById("copyAllKey");
+  const copyAllValue = document.getElementById("copyAllValue");
+  const allBtn = document.getElementById("allBtn");
 
-  cookiesListClickAddEventListener.addEventListener('click', (e) => {
-    // console.dir(e.target);
-    const textContent = e.target.textContent
-    if (e.target.title)
-      copy(e.target.title)
+  cookiesListClickAddEventListener.addEventListener("click", (e) => {
+    const textContent = e.target.textContent;
+    if (e.target.title) copy(e.target.title);
 
-    e.target.style.color = 'greenyellow'
-    e.target.innerText = '复制成功'
+    // e.target.style.color = "greenyellow";
+    e.target.style.color = successObject.color;
+    e.target.innerText = successObject.text;
+    // e.target.style.pointerEvents = 'none'
 
     time = setTimeout(() => {
-      e.target.style.color = 'var(--text-color)'
-      e.target.innerText = textContent
-    }, 1000)
-  })
+      e.target.style.color = "var(--text-color)";
+      e.target.innerText = textContent;
+      // e.target.style.pointerEvents = 'auto'
+    }, 1000);
+  });
 
+  copyAll.addEventListener("click", () => {
+    let all = "";
+    if (allTime) {
+      clearTimeout(allTime);
+      allTime = null;
+    }
+
+    myUrlList.forEach(({ name, value }) => {
+      all += `${name}:${value};\n`;
+    });
+
+    if (all) copy(all);
+
+    copyAll.innerText = successObject.text;
+    // copyAll.classList.add("disable");
+    copyAll.style.color = successObject.color;
+    copyAll.setAttribute("disable", true);
+    copyAll.style.cursor = "not-allowed";
+    copyAll.style.transform = "translateY(0)";
+
+    allTime = setTimeout(() => {
+      copyAll.innerText = "Copy All";
+      copyAll.style.color = "var(--btn-text-color)";
+      // copyAll.removeAttribute("disable");
+      copyAll.style.cursor = "pointer";
+      copyAll.style.transform = "translateY(1px)";
+      copyAll.classList.remove("disable");
+      clearTimeout(allTime);
+    }, 1000);
+  });
+
+  copyAllKey.addEventListener("click", () => {
+    let allKey = "";
+    if (allKeyTime) {
+      clearTimeout(allKeyTime);
+      allKeyTime = null;
+    }
+    myUrlList.forEach(({ name, value }) => {
+      allKey += `${name};\n`;
+    });
+
+    if (allKey) copy(allKey);
+
+    copyAllKey.innerText = successObject.text;
+    // copyAll.classList.add("disable");
+    copyAllKey.style.color = successObject.color;
+    copyAllKey.setAttribute("disable", true);
+    copyAllKey.style.cursor = "not-allowed";
+    copyAllKey.style.transform = "translateY(0)";
+
+    allKeyTime = setTimeout(() => {
+      copyAllKey.innerText = "Copy All Key";
+      copyAllKey.style.color = "var(--btn-text-color)";
+      // copyAll.removeAttribute("disable");
+      copyAllKey.style.cursor = "pointer";
+      copyAllKey.style.transform = "translateY(1px)";
+      copyAllKey.classList.remove("disable");
+      clearTimeout(allKeyTime);
+    }, 1000);
+  });
+
+  copyAllValue.addEventListener("click", () => {
+    let allValue = "";
+    if (allValueTime) {
+      clearTimeout(allValueTime);
+      allValueTime = null;
+    }
+    myUrlList.forEach(({ name, value }) => {
+      allValue += `${value};\n`;
+    });
+
+    if (allValue) copy(allValue);
+
+    copyAllValue.innerText = successObject.text;
+    // copyAll.classList.add("disable");
+    copyAllValue.style.color = successObject.color;
+    copyAllValue.setAttribute("disable", true);
+    copyAllValue.style.cursor = "not-allowed";
+    copyAllValue.style.transform = "translateY(0)";
+
+    allValueTime = setTimeout(() => {
+      copyAllValue.innerText = "Copy All Key";
+      copyAllValue.style.color = "var(--btn-text-color)";
+      // copyAll.removeAttribute("disable");
+      copyAllValue.style.cursor = "pointer";
+      copyAllValue.style.transform = "translateY(1px)";
+      copyAllValue.classList.remove("disable");
+      clearTimeout(allValueTime);
+    }, 1000);
+  });
 };
 
 init();
